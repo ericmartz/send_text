7d5e30cbceda91bf022b1cfb1cda298884f6ca2e
unknown <EMartz@BOG082196.bankga.com>
2015-04-13 12:12:44 -0400

    Changed send_message templates, removed instance variables from send_message.rb

--- app/mailers/send_message.rb
+++ app/mailers/send_message.rb
@@ -2,24 +2,21 @@ class SendMessage < ActionMailer::Base
   default from: "notifications@bankofgeorgia.com"
 
   def prep_sender(message)
-  	@message = message
-  	@contacts = Contact.all
-  	@contacts.each do |contact|
+  	contacts = Contact.all
+  	contacts.each do |contact|
   		if contact.carrier == "Verizon"
   			email = contact.phone_num.to_s + "@vtext.com"
-  			subject = "Verizon Found"
   		elsif contact.carrier == "AT&T"
   			email = contact.phone_num.to_s + "@txt.att.net"
-  			subject = "AT&T Found"
+  		elsif contact.carrier == "T-Mobile"
+  			email = contact.phone_num.to_s + "@tmomail.net"
   		end	
-  		SendMessage.send_message(@message, subject, email).deliver
+  		SendMessage.send_message(message, "Notification from The Bank of Georgia", email).deliver
   	end
   end
 
   def send_message(message, subject, contact)
   	@message = message
-  	@subject = subject
-  	@contact = contact
-  	mail(to: @contact, subject: @subject, message: @message.message_body)
+  	mail(to: contact, subject: subject, message: @message.message_body)
   end
 end
