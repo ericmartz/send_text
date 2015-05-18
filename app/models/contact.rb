88c73984cc728f5be49ef40d8ca03cd1e9ea65c8
Eric Martz <ericmartz@gmail.com>
2015-05-18 12:58:58 -0400

    Working on validating phone number properly.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,11 +4,19 @@ class Contact < ActiveRecord::Base
 	# This ensures that there are at least 10 characters in the phone number, but allows more than 10 characters through.
 	# Researching this, but wanted to move on to other parts and not get stuck on one thing.
 	# Added note, as I was reading through Ruby's Active Record Validations I came across using an if statment and a method 
-	#   for validations.  Check out second 5.  I might be able to do a validation if it matches at least 10 digits but not if it matches 11 digits.
+	#   for validations.  Check out section 6.  I might be able to do a validation if it matches at least 10 digits but not if it matches 11 digits.
 	#   http://guides.rubyonrails.org/active_record_validations.html#validation-helpers
 	validates_format_of :phone_num, with: /\d{10,10}/, message: "phone number must be 10 digits"
 	
 	# Would be nice if the error message returned the list of valid carriers.  Will need to include this eventually
 	validates :carrier, inclusion: { in: %w(Verizon AT&T Sprint T-Mobile MetroPCS Virgin Boost U.S. Cellular), 
 		                             message: "must be a valid carrier." }
+
+	
+
+	# validate :validates_phone_number
+	
+	# def validates_phone_number
+	# 	errors.add(:phone_num, "must be 10 digits") if :phone_num === /\d{10}/
+	# end
 end
